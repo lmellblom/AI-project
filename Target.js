3cf69207c44197edfe9d12d2428ade6f29142279
micnil <michaelnilsson90@hotmail.com>
2015-10-28 15:08:33 +0100

    added prawn sprite and modified is movement

--- Target.js
+++ Target.js
@@ -2,7 +2,7 @@ var Target = function (game, x, y) {
 
 	this.game = game;
 	// Inherit from sprite
-	Phaser.Sprite.call(this, game, x, y, 'diamond');
+	Phaser.Sprite.call(this, game, x, y,  'seacreatures', 'prawn0000');
 	this.scale.setTo(0.3);
 	this.anchor.set(0.5);
 	this.isActive = true;	// to be able to know if the target is taken or not.
@@ -11,20 +11,22 @@ var Target = function (game, x, y) {
 	this.radius = Math.max(this.height,this.width)/2.0; // the sprite itself has a width and a height
 														// use this in order to determine the radiue
 
-
-	//this.sprite = GAME.game.add.sprite(this.pos.x, this.pos.y, 'diamond');
+	this.scale.x = (Math.random() > 0.5) ? -0.3 : 0.3;
+	this.bounceFunction = (Math.random() > 0.5) ? Math.sin : Math.cos;
+	this.strafeFunction = (Math.random() > 0.5) ? Math.sin : Math.cos;
+	this.timeVariable = 0;
 }
 
 Target.prototype = Object.create(Phaser.Sprite.prototype);
 Target.prototype.constructor = Target;
 
 Target.prototype.move = function(dt) {
-	// standing still
+	this.timeVariable = (this.timeVariable>2*Math.PI) ? 0 : this.timeVariable+dt;
+	this.position.y += this.bounceFunction(this.timeVariable)/8;
+	this.position.x += this.strafeFunction(this.timeVariable)/6;
 };
 
 Target.prototype.setRandomPosition = function(stage){
-	//TODO: here we need to set the targets position withtin the boundaries of the stage.
-	//in the meentime:
 	this.x = getRandomInt(40, WIDTH-40);
 	this.y = getRandomInt(40, HEIGHT-40);
 	this.position.x = this.x;
